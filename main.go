34c96659c5387f3a216047a42acf5d609da479d7
infamous55 <infamouslegend555@gmail.com>
2023-05-01 17:52:09 +0300

    added test http server

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
 
 	cache "github.com/infamous55/go-zestful/cache"
@@ -22,9 +24,39 @@ func main() {
 		os.Exit(2)
 	}
 
-	_, err := cache.New(evictionPolicy, capacity)
+	newCache, err := cache.New(evictionPolicy, capacity)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v: initialization error\n", err)
 		os.Exit(2)
 	}
+
+	injectCache := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				ctx := context.WithValue(r.Context(), "cache", newCache)
+				next.ServeHTTP(w, r.WithContext(ctx))
+			},
+		)
+	}
+
+	http.Handle("/test", injectCache(http.HandlerFunc(testHandler)))
+	http.ListenAndServe(":8080", nil)
+}
+
+func getCache(ctx context.Context) cache.Cache {
+	if cache, ok := ctx.Value("cache").(cache.Cache); ok {
+		return cache
+	}
+	return nil
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	cache := getCache(ctx)
+	cache.Set("hello", "world")
+	value, err := cache.Get("hello")
+	if err != nil {
+		fmt.Fprint(w, err)
+	}
+	fmt.Fprint(w, value)
 }
